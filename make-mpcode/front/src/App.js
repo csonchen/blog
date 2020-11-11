611282ace850ac53e68d8f510ce4ab8bc8527c9b
csonchen <cson_chensheng@163.com>
2020-11-11 15:56:22 +0800

    feat: 编写表单静态页

--- make-mpcode/front/src/App.js
+++ make-mpcode/front/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { Button, Col, Form } from 'react-bootstrap'
+import './styles/bootstrap.mini.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>生成小程序服务</h3>
+      <br />
+      <Form.Group>
+        <Form.Row className="form-item">
+          <Form.Label column lg={2}>
+            小程序appid
+          </Form.Label>
+          <Col>
+            <Form.Control type="text" placeholder="Normal text" />
+          </Col>
+        </Form.Row>
+        <Form.Row className="form-item">
+          <Form.Label column lg={2}>
+            AppScret
+          </Form.Label>
+          <Col>
+            <Form.Control type="text" placeholder="Normal text" />
+          </Col>
+        </Form.Row>
+        <Form.Row className="form-item">
+          <Form.Label column lg={2}>
+            页面地址
+          </Form.Label>
+          <Col>
+            <Form.Control type="text" placeholder="Normal text" />
+          </Col>
+        </Form.Row>                
+      </Form.Group>
+
+      <Button variant="primary">点击生成小程序码</Button>
     </div>
-  );
+  )
 }
 
 export default App;
