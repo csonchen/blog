fe4a3c7c3fe93c349b697f12860768de6e827ae6
csonchen <cson_chensheng@163.com>
2020-11-12 15:06:40 +0800

    fe: 编写构建小程序码图片接口

--- make-mpcode/server/src/app.js
+++ make-mpcode/server/src/app.js
@@ -1,16 +1,43 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const { APP_ID } = require('./config');
+const ci = require('miniprogram-ci');
 
+// test api
 app.get('/', (req, res) => res.send('hello world'))
 
-app.get('/api/customers', (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'john', lastName: 'doe'},
-    {id: 2, firstName: 'brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'sam', last: 'chen'}
-  ]
-  res.json(customers)
+// 允许访问static目录资源
+app.use('/static', express.static(path.join(__dirname, 'static')))
+
+// 构建小程序，生成预览码图片
+app.get('/api/preview', async (req, res) => {
+  const project = new ci.Project({
+    appid: APP_ID,
+    type: 'miniProgram',
+    projectPath: '../wx-mall-components/dist',
+    privateKeyPath: 'keys/private.wx5a35be4e15614ade.key',
+    ignores: ['node_modules/**/*'],
+  })
+  const previewResult = await ci.preview({
+    project,
+    desc: 'hello',
+    setting: {
+      ex6: true,
+    },
+    qrcodeFormat: 'image',
+    qrcodeOutputDest: 'src/static/preview.jpg',
+    onProgressUpdate: console.log,
+  })
+  res.json({
+    code: 200,
+    message: '操作成功',
+    data: {
+      previewImg: 'http://localhost:5000/static/preview.jpg',
+    }
+  })
 })
+
 
 const port = 5000;
 app.listen(port, () => console.log(`make mpcode service listen on port ${5000}`))
