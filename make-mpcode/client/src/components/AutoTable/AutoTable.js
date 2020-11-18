df08d7b91db34e546c62c71dd55ae9f48d6a91fe
csonchen <cson_chensheng@163.com>
2020-11-18 15:43:36 +0800

    re: 抽离公共表格组件

--- make-mpcode/client/src/components/AutoTable/AutoTable.js
+++ make-mpcode/client/src/components/AutoTable/AutoTable.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Pagination, Spinner, Table } from 'react-bootstrap'
+import { range } from '../../tools/util'
 
 const AutoTable = (props) => {
-  const { heads, loading, pages } = props || {}
+  const PAGE_SIZE = 10
+  const [pages, setPages] = useState([])
+  const { heads, loading, tableList, onPageClick, currentPage } = props || {}
+
+  useEffect(() => {
+    const size = Math.ceil(tableList.length / PAGE_SIZE)
+    setPages(() => range(1, size))
+  }, [tableList])
+
+  const queryByPage = (e) => {
+    const pageNum = e.target.dataset.page
+    const begin = (+pageNum - 1) * PAGE_SIZE
+    const end = begin + PAGE_SIZE
+    const currentList = tableList.slice(begin, end)
+    props.setCurrentList && props.setCurrentList(currentList, +pageNum)
+  }
   return (
     <>
       <Table striped bordered hover>
@@ -10,15 +26,15 @@ const AutoTable = (props) => {
         <thead>
           <tr>
             {heads.map((item, index) => (
-              <th key={index}>{item}</th>
+            <th key={index}>{item}</th>
             ))}
           </tr>
         </thead>
         }
-        <thody>
+        <tbody>
           {loading ?
           <tr>
-            <td colSpane={heads.length} className="text-center">
+            <td colSpan={heads.length} className="text-center">
               <Spinner animation="border" variant="primary" role="status">
                 <span className="sr-only">加载中...</span>
               </Spinner>
@@ -27,14 +43,15 @@ const AutoTable = (props) => {
           :
           props.children
           }
-        </thody>
+        </tbody>
       </Table>
       <div className="flex-end">
-        <Pagination>
+        <Pagination onClick={onPageClick || queryByPage}>
         {pages.map((page, index) => (
           <Pagination.Item
             key={index}
             data-page={page}
+            active={currentPage === page}
           >{page}</Pagination.Item>
         ))}
         </Pagination>
--- make-mpcode/client/src/pages/AnalyseComponentsPage/AnalyseComponentsPage.js
+++ make-mpcode/client/src/pages/AnalyseComponentsPage/AnalyseComponentsPage.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import { Col, Form, Pagination, Spinner, Table } from 'react-bootstrap'
+import { Col, Form } from 'react-bootstrap'
+import AutoTable from '../../components/AutoTable/AutoTable'
 import LoadingButton from '../../components/LoadingButton/LoadingButton'
 import { postData } from '../../services/request'
-import { range } from '../../tools/util'
-
 export default class AnalyseComponentsPage extends React.Component {
-  PAGE_SIZE = 10
   originTableList = []
   state = {
     loading: false,
@@ -13,7 +11,6 @@ export default class AnalyseComponentsPage extends React.Component {
     tableList: [],
     currentList: [],
     currentPage: 1,
-    pages: [],
   }
   analyseComponents = () => {
     this.setState({
@@ -23,30 +20,22 @@ export default class AnalyseComponentsPage extends React.Component {
       const { list: tableList } = res.data
       this.originTableList = tableList
       const currentList = tableList.slice(0, 10)
-      const size = Math.ceil(tableList.length / this.PAGE_SIZE)
-      const pages = range(1, size)
       this.setState({
         tableList,
         currentList,
-        pages,
         loading: false,
         currentPage: 1,
       })
     })
   }
-  queryByPage = (e) => {
-    const pageNum = e.target.dataset.page
-    const { tableList } = this.state
-    const begin = (+pageNum - 1) * this.PAGE_SIZE
-    const end = begin + this.PAGE_SIZE
-    const currentList = tableList.slice(begin, end)
+  setCurrentList = (currentList, currentPage) => {
     this.setState({
-      currentPage: +pageNum,
+      currentPage,
       currentList,
     })
   }
   render() {
-    const { loading, operateLoading } = this.state
+    const { loading, operateLoading, tableList, currentPage } = this.state
     return (
       <div>
         <Form.Group>
@@ -56,57 +45,30 @@ export default class AnalyseComponentsPage extends React.Component {
               <Form.Control type="text" placeholder="" />
             </Col>
           </Form.Row>
-
           <div className="flex-end">
             <LoadingButton className="mr10" loading={loading} onClick={this.analyseComponents}>开始分析</LoadingButton>
             <LoadingButton loading={operateLoading}>导出</LoadingButton>
           </div>
         </Form.Group>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>序号</th>
-              <th>页面</th>
-              <th>当前目录</th>
-              <th>引入组件</th>
-              <th>组件路径</th>
-              <th>使用情况</th>
+        <AutoTable 
+          heads={['序号', '页面', '当前目录', '引入组件', '组件路径', '使用情况']}
+          loading={loading}
+          tableList={tableList}
+          currentPage={currentPage}
+          setCurrentList={this.setCurrentList}
+        >
+          {this.state.currentList.map((item, index) => (
+            <tr key={index}>
+              <td>{item.id}</td>
+              <td>{item.page}</td>
+              <td>{item.directory}</td>
+              <td>{item.component}</td>
+              <td>{item.componentPath}</td>
+              <td>{item.used}</td>
             </tr>
-          </thead>
-          <tbody>
-            {this.state.loading ?
-            <tr>
-              <td colSpan="6" className="text-center">
-                <Spinner animation="border" variant="primary" role="status">
-                  <span className="sr-only">Loading...</span>
-                </Spinner>
-              </td>
-            </tr>
-            :
-            this.state.currentList.map((item, index) => (
-              <tr key={index}>
-                <td>{item.id}</td>
-                <td>{item.page}</td>
-                <td>{item.directory}</td>
-                <td>{item.component}</td>
-                <td>{item.componentPath}</td>
-                <td>{item.used}</td>
-              </tr>
-            ))
-            }
-          </tbody>
-        </Table>
-        <div className="flex-end">
-          <Pagination onClick={this.queryByPage}>
-            {this.state.pages.map((page, index) => (
-            <Pagination.Item
-              data-page={page}
-              key={index} 
-              active={this.state.currentPage === page}
-            >{page}</Pagination.Item>
-            ))}
-          </Pagination>
-        </div>
+          ))
+          }
+        </AutoTable>
       </div>
     )
   }
